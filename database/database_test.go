54ba2acc787da1bf3a426ed0b79721dee17eefff
temwalker <sutep.nnt@gmail.com>
2022-12-29 02:09:31 +0700

    correct Get DB Error test case

--- database/database_test.go
+++ database/database_test.go
@@ -1,6 +1,9 @@
+//go:build unit
+
 package database
 
 import (
+	"database/sql/driver"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -21,7 +24,15 @@ func TestDatabase(t *testing.T) {
 		assert.NoError(t, err)
 	})
 	t.Run("Get DB Error", func(t *testing.T) {
-		_, err := GetDB()
+		db, mock, err := sqlmock.New(sqlmock.MonitorPingsOption(true))
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+		mock.ExpectPing().WillReturnError(driver.ErrBadConn)
+		d, _ := GetDB()
+		d.Database = db
+		_, err = GetDB()
+		defer d.CloseDB()
 		assert.Error(t, err)
 	})
 }
